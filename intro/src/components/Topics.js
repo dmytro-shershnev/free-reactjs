485a918201feebdfc179be730fa7d544ca00ddfb
Dmytro Shershnev <Dmytro_Shershnev@epam.com>
2018-08-02 17:33:13 +0300

    FR-0020: Add routing to the project.

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
-export default function Topic() {
-    return <div>TOPICS</div>;
+function Topic({ match }) {
+    return <h3>{match.params.topicId}</h3>;
+}
+
+export default function Topics() {
+    return (
+        <div>
+            <h2>TOPICS</h2>
+            <ul>
+                <li>
+                    <Link to='/topics/rendering'>Rendering with React</Link>
+                </li>
+                <li>
+                    <Link to='/topics/components'>Components</Link>
+                </li>
+                <li>
+                    <Link to='/topics/props-v-state'>Props vs State</Link>
+                </li>
+            </ul>
+
+            <hr />
+
+            <Route path='/topics/:topicId' component={Topic} />
+        </div>
+    );
 }
